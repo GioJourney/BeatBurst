d714624eba15e1c3ea9f5f118152f1687c5a6da0
Giovanni <giovanni.loiacono182@gmail.com>
2025-10-30 23:40:57 +0100

    minor update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   // Legal compliance
   userAcceptedTerms = false;
   showLegalNotice = true;
-  readonly LEGAL_VERSION = '1.0.0';
+  readonly LEGAL_VERSION = '1.0.1';
 
   private subscriptions = new Subscription();
   private animations: AnimeInstance[] = [];
